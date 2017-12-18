32dce7a02b0379a67986e89f1afbc78676705193
huangsen <huangsen125@163.com>
2017-12-18 10:29:21 +0800

    2017-12-18

--- src/pkg/smp/mlib/entry.go
+++ src/pkg/smp/mlib/entry.go
@@ -1,9 +1,9 @@
 package library
 
-type Music struct{
-	Id string
-	Name string
+type MusicEntry struct {
+	Id     string
+	Name   string
 	Artist string
 	Source string
-	Type string
+	Type   string
 }
